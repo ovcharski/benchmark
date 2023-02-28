86fb4d4853b7cfaa9115f478a231f7cea0c46e7d
Nikola Ovcahrski Laptop <nikolaovcharski@gmail.com>
2023-02-28 14:48:36 +0200

    timeout increase

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 30 * 2000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 10000
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
