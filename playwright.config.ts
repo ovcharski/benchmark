7b497d74ae67209065ddabbd0822f7f1d7f5e5c3
ovcharski <48056945+ovcharski@users.noreply.github.com>
2025-01-31 10:11:24 +0200

    headless: true

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        headless: false,
+        headless: true,
         viewport: { width: 1920, height: 1080 },
         launchOptions: {
           args: ['--start-maximized', '--start-fullscreen']
